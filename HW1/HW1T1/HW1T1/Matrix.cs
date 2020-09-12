7b27a50c4daa67389ba83c454ed266d59a21f665
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-09-12 12:45:30 +0300

    common commit

---
+++ HW1/HW1T1/HW1T1/Matrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace HW1T1
+{
+    public class Matrix
+    {
+        public int[,] matrix { private set; get; }
+
+        public void LoadAndCreateMatrix(string fileName)
+        {
+            var queueOfRows = new Queue<string>();
+
+            using (var sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    queueOfRows.Enqueue(sr.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < queueOfRows.Count; i++)
+            {
+
+            }
+        }
+    }
+}
