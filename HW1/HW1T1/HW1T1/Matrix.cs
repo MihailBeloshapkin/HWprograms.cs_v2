41228063a1b90409bcd63a1b3e539621d9350350
Ржевка <misha.beloshapkin@yandex.ru>
2020-09-13 10:45:33 +0300

    added matrix loading

--- HW1/HW1T1/HW1T1/Matrix.cs
+++ HW1/HW1T1/HW1T1/Matrix.cs
@@ -5,10 +5,21 @@ using System.IO;
 
 namespace HW1T1
 {
+    /// <summary>
+    /// This class contains matrix and its functions.
+    /// </summary>
     public class Matrix
     {
-        public int[,] matrix { private set; get; }
+        public int[,] matrix { private set; get; } = null;
 
+        public Matrix(string fileName)
+        {
+            this.LoadAndCreateMatrix(fileName);
+        }
+
+        /// <summary>
+        /// Load matrix from txt file and create.
+        /// </summary>
         public void LoadAndCreateMatrix(string fileName)
         {
             var queueOfRows = new Queue<string>();
@@ -21,10 +32,74 @@ namespace HW1T1
                 }
             }
 
-            for (int i = 0; i < queueOfRows.Count; i++)
-            {
+            var currentRow = queueOfRows.Dequeue();
+            var currentIntegerRow = new Queue<int>();
+            int jiter = 0;
 
+            while (jiter < currentRow.Length)
+            {
+                if (!char.IsDigit(currentRow[jiter]) && currentRow[jiter] != ' ' && currentRow[jiter] != '\n' && currentRow[jiter] != '\r')
+                {
+                    throw new Exception();
+                }
+
+                string number = null;
+
+                while (jiter < currentRow.Length && currentRow[jiter] != ' ')
+                {
+                    number += currentRow[jiter];
+                    jiter++;
+                }
+
+                if (number != null)
+                {
+                    currentIntegerRow.Enqueue(int.Parse(number));
+
+                }
+                jiter++;
             }
+
+            this.matrix = new int[queueOfRows.Count + 1, currentIntegerRow.Count];
+
+            for (int i = 0; currentIntegerRow.Count > 0; i++)
+            {
+                matrix[0, i] = currentIntegerRow.Dequeue();
+            }
+
+            for (int i = 1; queueOfRows.Count > 0; i++)
+            {
+                currentRow = queueOfRows.Dequeue();
+
+                int iter = 0;
+                int rowIndex = 0;
+
+                while (iter < currentRow.Length)
+                {
+                    string number = null;
+                    
+                    if (!char.IsDigit(currentRow[iter]) && currentRow[iter] != ' ' && currentRow[iter] != '\n' && currentRow[iter] != '\r')
+                    {
+                        throw new Exception();
+                    }
+
+                    while (iter < currentRow.Length && currentRow[iter] != ' ')
+                    {
+                        number += currentRow[iter];
+                        iter++;
+                    }
+
+                    if (number != null)
+                    {
+                        this.matrix[i, rowIndex] = int.Parse(number);
+                    }
+
+                    rowIndex++;
+                    iter++;
+                }
+            }
+
         }
+
+
     }
 }
--- HW1/HW1T1/HW1T1/Program.cs
+++ HW1/HW1T1/HW1T1/Program.cs
@@ -1,11 +1,55 @@
 ﻿using System;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.WindowsRuntime;
 
 namespace HW1T1
 {
     class Program
     {
+        private void Display(int[,] matrix)
+        { 
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); i++)
+                {
+                    Console.Write("{0, 3}", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private int MultiplyRowAndColumn(int[,] matrix1, int[,] matrix2, int i, int j)
+        {
+            int result = 0;
+
+            for (int iter = 0; iter < matrix1.GetLength(0); iter++)
+            {
+
+            }
+
+            return result;
+        }
+
+        private void Multiplication(int[,] matrix1, int[,] matrix2)
+        {
+            int[,] matrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+
+                }
+            }
+
+            
+        }
+
         static void Main(string[] args)
         {
+            int[,] matrix1 = new int[2, 3] { {1, 4, 2 }, {7, 2, 5 } };
+            int[,] matrix2 = new int[3, 2] { { 0, 3 }, { 1, 7 }, { 5, 3 } };
+
+            var matrix = new Matrix("../../../matrix1.txt");
             Console.WriteLine("Hello World!");
         }
     }
