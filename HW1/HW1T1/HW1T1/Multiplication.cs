7c6579d207b44f14bd6c862e29ee32c65faf94f8
Ржевка <misha.beloshapkin@yandex.ru>
2020-09-13 18:01:39 +0300

    Add parallel algorithm

--- HW1/HW1T1/HW1T1/Multiplication.cs
+++ HW1/HW1T1/HW1T1/Multiplication.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 
 namespace HW1T1
 {
@@ -36,6 +37,51 @@ namespace HW1T1
                 {
                     result.matrix[i, j] = MultiplyRowAndColumn(matrix1, matrix2, i, j);
                 }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// The same algorithm as in the SimpleMultiplication, but executes by multithreading.
+        /// </summary>
+        /// <returns>Result of the multiplication.</returns>
+        public static Matrix ParallelMultiplication(Matrix matrix1, Matrix matrix2)
+        {
+            if (matrix1.CountOfColumns != matrix2.CountOfRows)
+            {
+                throw new Exception();
+            }
+
+            var result = new Matrix(matrix1.CountOfRows, matrix2.CountOfColumns);
+            var threads = new Thread[Environment.ProcessorCount];
+
+            var chunkSize = result.CountOfRows / threads.Length + 1;
+
+            for (int iter = 0; iter < threads.Length; iter++)
+            {
+                var localI = iter;
+                threads[localI] = new Thread(() =>
+                {
+                    for (int i = localI * chunkSize; i < (localI + 1) * chunkSize && i < result.CountOfRows; i++)
+                    {
+                        for (int j = 0; j < result.CountOfColumns; j++)
+                        {
+                            result.matrix[i, j] = MultiplyRowAndColumn(matrix1, matrix2, i, j);
+                        }
+                    }
+                }
+               );
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
             }
 
             return result;
--- HW1/HW1T1/HW1T1/Program.cs
+++ HW1/HW1T1/HW1T1/Program.cs
@@ -10,8 +10,10 @@ namespace HW1T1
         {
             var matrix1 = new Matrix("../../../matrix1.txt");
             var matrix2 = new Matrix("../../../matrix2.txt");
-            var result = Multiplication.SimpleMultiplication(matrix1, matrix2);
-            result.Display();
+            var result1 = Multiplication.SimpleMultiplication(matrix1, matrix2);
+            var result2 = Multiplication.ParallelMultiplication(matrix1, matrix2);
+            result1.Display();
+            result2.Display();
         }
     }
 }
