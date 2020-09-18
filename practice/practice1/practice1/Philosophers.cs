e0944f8295028ddf863e9412c456eea5afbacbe7
mihail.beloshapkin <misha.beloshapkin@yandex.ru>
2020-09-18 18:19:45 +0300

    create

---
+++ practice/practice1/practice1/Philosophers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace practice1
+{
+    public class Philosophers
+    {
+        public Philosophers()
+        {
+
+        }
+    }
+}
