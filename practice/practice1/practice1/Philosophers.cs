c025bd15bd5d5325babe2704064909f26cd50b70
Ржевка <misha.beloshapkin@yandex.ru>
2020-09-20 23:43:34 +0300

    common commit

--- practice/practice1/practice1/Philosophers.cs
+++ practice/practice1/practice1/Philosophers.cs
@@ -1,14 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace practice1
 {
     public class Philosophers
     {
-        public Philosophers()
+        public Philosophers(List<Object> forks, int num)
         {
-
+            this.forks = forks;
+            this.num = num;
         }
+
+        private const int MaxTime = 10;
+
+        private readonly int num;
+        private readonly List<Object> forks;
+        private static readonly Random Random = new Random();
+        private volatile bool isDead;
+
+        public void Live()
+        {
+            while(!isDead)
+            {
+                var time = Random.Next(MaxTime);
+                Console.WriteLine($"Philosopher {num} is thinking for {time} milliseconds.");
+                Thread.Sleep(time);
+
+                var firstFork = num == forks.Count - 1 ? 0 : num;
+                var secondFork = num == forks.Count - 1 ? num : num + 1;
+
+                time = Random.Next(MaxTime);
+                Console.WriteLine($"Philosopher {num} is trying to take {firstFork} fork.");
+                lock (forks[firstFork])
+                {
+                    Console.WriteLine($"Philosopher {num} is trying to take {secondFork} fork.");
+                    lock (forks[secondFork])
+                    {
+                        Console.WriteLine($"Philosopher {num} is eating for {time} milliseconds.");
+                        Thread.Sleep(time);
+                    }
+                }
+            }
+        }
+
+        public void Kill()
+        {
+            this.isDead = false;
+        }
+
     }
 }
--- practice/practice1/practice1/Program.cs
+++ practice/practice1/practice1/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace practice1
 {
     class Program
     {
+        private const int PhilosophersCount = 2;
+
         static void Main(string[] args)
         {
-            var philosophers = new List<Philosophers>();
+            var philosophers = new List<Philosophers>(PhilosophersCount);
+            var forks = new List<object>(PhilosophersCount);
+
+            for (var i = 0; i < PhilosophersCount; i++)
+            {
+                philosophers.Add(new Philosophers(forks, i));
+                forks.Add(new object());
+            }
+
+            var threads = new Thread[PhilosophersCount];
+            for (var i = 0; i < PhilosophersCount; i++)
+            {
+                var localI = i;
+                threads[i] = new Thread(() => philosophers[localI].Live());
+                threads[i].Start();
+            }
+
+            Console.ReadLine();
+            foreach (var philosopher in philosophers)
+            {
+                philosopher.Kill();
+            }
+
+            for (var i = 0; i < PhilosophersCount; i++)
+            {
+                threads[i].Join();
+            }
 
         }
     }
