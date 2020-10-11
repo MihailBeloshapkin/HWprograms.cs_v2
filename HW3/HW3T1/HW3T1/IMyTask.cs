7ad202efcfc993c5caa1d3785199c8e57b69635f
Ржевка <misha.beloshapkin@yandex.ru>
2020-10-11 17:48:32 +0300

    some changes

---
+++ HW3/HW3T1/HW3T1/IMyTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW3T1
+{
+    public interface IMyTask<out TResult>
+    {
+        bool IsCompleted { get; }
+
+        TResult Result { get; }
+
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
---
+++ HW3/HW3T1/HW3T1/MyThreadPool.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace HW3T1
+{
+    /// <summary>
+    /// Thread pool.
+    /// </summary>
+    public class MyThreadPool
+    {
+        public MyThreadPool(int countOfThreads)
+        {
+            this.countOfThreads = countOfThreads;
+            this.Threads = new Thread[countOfThreads];
+            cancellationTokenSource = new CancellationTokenSource();
+            newTasks = new Queue<Action>();
+            this.Start();
+        }
+
+        private int countOfThreads = 0;
+
+        private Thread[] Threads = null;
+
+        private readonly CancellationTokenSource cancellationTokenSource = null;
+
+        private readonly Queue<Action> newTasks = null;
+
+        private void Start()
+        {
+            foreach (var thread in Threads)
+            {
+            }
+        }
+
+        /// <summary>
+        /// Interrupts all executing tasks.
+        /// </summary>
+        public void ShutDown()
+            => cancellationTokenSource.Cancel();
+    }
+}
