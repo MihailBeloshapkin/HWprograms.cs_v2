a8472f0c24b0905d0ff2912da03dcd9f8ebf0a46
Ржевка <misha.beloshapkin@yandex.ru>
2020-10-17 18:48:56 +0300

    add some methods

--- HW3/HW3T1/HW3T1/MyThreadPool.cs
+++ HW3/HW3T1/HW3T1/MyThreadPool.cs
@@ -15,8 +15,8 @@ namespace HW3T1
         {
             this.countOfThreads = countOfThreads;
             this.Threads = new Thread[countOfThreads];
-            cancellationTokenSource = new CancellationTokenSource();
-            Tasks = new ThreadQueue<Action>();
+            this.cancellationTokenSource = new CancellationTokenSource();
+            this.Tasks = new ThreadQueue<Action>();
             this.Start();
         }
 
@@ -37,6 +37,9 @@ namespace HW3T1
             }
         }
 
+        /// <summary>
+        /// Execute current.
+        /// </summary>
         public void Execute(CancellationToken cancellationToken)
         {
             while (true)
@@ -55,9 +58,15 @@ namespace HW3T1
         /// <summary>
         /// Interrupts all executing tasks.
         /// </summary>
-        public void ShutDown()
+        public void Shutdown()
             => cancellationTokenSource.Cancel();
 
+        
+
+        /// <summary>
+        /// Submit new function.
+        /// </summary>
+        /// <param name="func">Input function</param>
         public IMyTask<TResult> Submit<TResult>(Func<TResult> func)
         {
             if (func == null)
@@ -65,9 +74,81 @@ namespace HW3T1
                 throw new ArgumentNullException();
             }
 
-            var newTask = new MyTask<TResult>(func);
+            var newTask = new MyTask<TResult>(func, this);
             this.Tasks.Enqueue(newTask.Execute);
-            return null;
+            return newTask;
+        }
+
+        private class MyTask<TResult> : IMyTask<TResult>
+        {
+            public MyTask(Func<TResult> func, MyThreadPool threadPool)
+            {
+                this.threadPool = threadPool;
+                this.func = func;
+                this.submitFunctionsQueue = new Queue<Action>();
+            }
+
+
+            public TResult Result
+            {
+                get
+                {
+                    return this.result;
+                }
+            }
+
+            public bool IsCompleted { get; set; } = false;
+
+            private MyThreadPool threadPool = null;
+
+            private Func<TResult> func;
+
+            private AggregateException aggregateException = null;
+
+            private Queue<Action> submitFunctionsQueue = null;
+
+            private TResult result = default(TResult);
+
+            private Object locker;
+
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> newFunc)
+            {
+                if (newFunc == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                lock (locker)
+                {
+                    if (this.IsCompleted)
+                    {
+                        return threadPool.Submit(() => newFunc(result));
+                    }
+                    var newTask = new MyTask<TNewResult>(() => newFunc(result), threadPool);
+                    this.submitFunctionsQueue.Enqueue(newTask.Execute);
+                    return newTask;
+                }
+            }
+
+            /// <summary>
+            /// Apply function to result.
+            /// </summary>
+            public void Execute()
+            {
+                try
+                {
+                    result = this.func();
+                }
+                catch (Exception e)
+                {
+                    this.aggregateException = new AggregateException(e);
+                }
+                finally
+                {
+                    this.IsCompleted = true;
+                }
+            }
         }
     }
 }
+
