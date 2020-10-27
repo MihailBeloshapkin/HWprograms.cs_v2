b1f8e044c3e305616cf7d7f236099225ba57f14c
Ржевка <misha.beloshapkin@yandex.ru>
2020-10-27 11:02:10 +0300

    create Server and Client classes

--- tests/Test1v1/Test1v1/Program.cs
+++ tests/Test1v1/Test1v1/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Threading;
+using System.Xml.Schema;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 
 namespace Test1v1
 {
     class Program
     {
+        static Server server;
+        static TcpClient client;
+        static NetworkStream stream;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int port = 0;
+            string ip;
+            
+            if (args.Length == 1)
+            {
+                port = int.Parse(args[0]);
+                try
+                {
+                    server = new Server();
+                    var ListenThread = new Thread(server.Listen);
+                    ListenThread.Start();
+                }
+                catch (Exception)
+                {
+                    server.Disconnect();
+                }
+            }
+            else 
+            {
+                port = int.Parse(args[0]);
+                ip = args[1];
+                try
+                {
+                    client = new TcpClient();
+                    client.Connect(ip, port);
+                    stream = client.GetStream();
+                }
+                catch (Exception)
+                {
+
+                }
+            }
         }
     }
 }
