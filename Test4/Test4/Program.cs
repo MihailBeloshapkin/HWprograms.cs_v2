6d241c2347ce517c652d59ef82db71895faeae4a
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2020-12-22 13:03:40 +0300

    add more tests

--- Test4/Test4/Program.cs
+++ Test4/Test4/Program.cs
@@ -6,36 +6,64 @@ namespace Test4
 {
     class Program
     {
-        private const int arraySize = 100000000;
+        private const int bigArraySize = 100000000;
+        private const int arraySize = 10000;
 
         static void Main(string[] args)
         {
             var stopwatch1 = new Stopwatch();
             var stopwatch2 = new Stopwatch();
 
-            int[] array = new int[arraySize];
-            for (int iter = 0; iter < array.Length; iter++)
+            int[] bigArray = new int[bigArraySize];
+            for (int iter = 0; iter < bigArray.Length; iter++)
             {
-                array[iter] = array.Length - iter;
+                bigArray[iter] = bigArray.Length - iter;
             }
 
             stopwatch1.Start();
-            QuickSort.SimpleQuickSort(array, 0, array.Length - 1);
+            QuickSort.SimpleQuickSort(bigArray, 0, bigArray.Length - 1);
             stopwatch1.Stop();
 
             
 
+            for (int iter = 0; iter < bigArray.Length; iter++)
+            {
+                bigArray[iter] = bigArray.Length - iter;
+            }
+
+            stopwatch2.Start();
+            QuickSort.ParallelQuickSort(bigArray, 0, bigArray.Length - 1);
+            stopwatch2.Stop();
+
+            Console.WriteLine($"Simple quick sort takes {stopwatch1.ElapsedMilliseconds} milliseconds");
+            Console.WriteLine($"Parallel quick sort takes {stopwatch2.ElapsedMilliseconds} milliseconds");
+
+            var stopwatch3 = new Stopwatch();
+            var stopwatch4 = new Stopwatch();
+
+            int[] array = new int[bigArraySize];
             for (int iter = 0; iter < array.Length; iter++)
             {
                 array[iter] = array.Length - iter;
             }
 
-            stopwatch2.Start();
-            QuickSort.ParallelQuickSort(array, 0, array.Length - 1);
-            stopwatch2.Stop();
+            stopwatch3.Start();
+            QuickSort.SimpleQuickSort(array, 0, array.Length - 1);
+            stopwatch3.Stop();
 
-            Console.WriteLine($"Simple quick sort takes {stopwatch1.ElapsedMilliseconds} milliseconds");
-            Console.WriteLine($"Parallel quick sort takes {stopwatch2.ElapsedMilliseconds} milliseconds");
+
+
+            for (int iter = 0; iter < array.Length; iter++)
+            {
+                array[iter] = array.Length - iter;
+            }
+
+            stopwatch4.Start();
+            QuickSort.ParallelQuickSort(array, 0, bigArray.Length - 1);
+            stopwatch4.Stop();
+            Console.WriteLine($"In case if array size is ");
+            Console.WriteLine($"Simple quick sort takes {stopwatch3.ElapsedMilliseconds} milliseconds");
+            Console.WriteLine($"Parallel quick sort takes {stopwatch4.ElapsedMilliseconds} milliseconds");
         }
     }
 }
