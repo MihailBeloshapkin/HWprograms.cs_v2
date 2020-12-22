ebc4feb6c6ce29a9cad530608986ba43bab7fc28
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2020-12-22 11:46:02 +0300

    create project

---
+++ Test4/Test4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 4, 3, 2, 1, 7, 6 };
+            QuickSort.ParallelQuickSort(array, 0, array.Length - 1);
+            for (int iter = 0; iter < array.Length; iter++)
+            {
+                Console.Write($"{array[iter]} ");
+            }
+        }
+    }
+}
---
+++ Test4/Test4/QuickSort.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Test4
+{
+	/// <summary>
+	/// This class contains diffrent realizations 
+	/// of the QuickSort method.
+	/// </summary>
+    public class QuickSort
+    {
+
+		/// <summary>
+		/// Simple realization.
+		/// </summary>
+		public static void SimpleQuickSort(int[] array, int head, int tail)
+        {
+			int indexOfElement = (head + tail) / 2;
+			int indexOfHead = head - 1;
+			int indexOfTail = tail + 1;
+			
+			if (head >= tail)
+			{
+				return;
+			}
+			while (indexOfHead < indexOfTail)
+			{
+				indexOfHead++;
+				while (array[indexOfHead] < array[indexOfElement])
+				{
+					indexOfHead++;
+				}
+				indexOfTail--;
+				while (array[indexOfTail] > array[indexOfElement])
+				{
+					indexOfTail--;
+				}
+
+				if (indexOfHead < indexOfTail)
+				{
+					int tmp = array[indexOfTail];
+					array[indexOfTail] = array[indexOfHead];
+					array[indexOfHead] = tmp;
+
+					if (indexOfElement == indexOfHead)
+					{
+						indexOfElement = indexOfTail;
+						indexOfTail++;
+					}
+					if (indexOfElement == indexOfTail)
+					{
+						indexOfElement = indexOfHead;
+						indexOfHead--;
+					}
+				}
+			}
+
+			SimpleQuickSort(array, head, indexOfElement - 1);
+			SimpleQuickSort(array, indexOfElement + 1, tail);
+		}
+
+		public static void ParallelQuickSort(int[] array, int head, int tail)
+		{
+			int indexOfElement = (head + tail) / 2;
+			int indexOfHead = head - 1;
+			int indexOfTail = tail + 1;
+			
+			if (head >= tail)
+			{
+				return;
+			}
+			while (indexOfHead < indexOfTail)
+			{
+				indexOfHead++;
+				while (array[indexOfHead] < array[indexOfElement])
+				{
+					indexOfHead++;
+				}
+				indexOfTail--;
+				while (array[indexOfTail] > array[indexOfElement])
+				{
+					indexOfTail--;
+				}
+
+				if (indexOfHead < indexOfTail)
+				{
+					int tmp = array[indexOfTail];
+					array[indexOfTail] = array[indexOfHead];
+					array[indexOfHead] = tmp;
+
+					if (indexOfElement == indexOfHead)
+					{
+						indexOfElement = indexOfTail;
+						indexOfTail++;
+					}
+					if (indexOfElement == indexOfTail)
+					{
+						indexOfElement = indexOfHead;
+						indexOfHead--;
+					}
+				}
+			}
+
+			var FirstThread = new Thread(() => ParallelQuickSort(array, head, indexOfElement - 1));
+			var SecondThread = new Thread(() => ParallelQuickSort(array, indexOfElement + 1, tail));
+			
+			FirstThread.Start();
+			SecondThread.Start();
+			
+			FirstThread.Join();
+			SecondThread.Join();
+		}
+    }
+}
