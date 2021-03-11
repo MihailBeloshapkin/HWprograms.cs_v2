52cd1d903602948a762280d1eadd25ea1d709d8f
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-03-11 22:48:50 +0300

    add some useful methods

--- HW4/HW4T1/Server/Server.cs
+++ HW4/HW4T1/Server/Server.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
+using System.IO;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 
 namespace Server
 {
@@ -16,5 +18,53 @@ namespace Server
             this.listener = new TcpListener(localAddress, port);
             this.listener.Start();
         }
+
+        public async Task Run()
+        {
+            while (true)
+            {
+                var socket = await listener.AcceptSocketAsync();
+                
+            }
+        }
+
+        /// <summary>
+        /// Listing
+        /// </summary>
+        private async Task List(string path, StreamWriter writer)
+        {
+            if (!Directory.Exists(path))
+            {
+                await writer.WriteLineAsync("-1");
+                return;
+            }
+
+            var listOfDirictories = Directory.GetDirectories(path);
+            var setOfFiles = Directory.GetFiles(path);
+
+            var response = (listOfDirictories.Length + setOfFiles.Length).ToString();
+
+            foreach (var direcotry in listOfDirictories)
+            {
+                response += $"{direcotry} -> directory";
+            }
+
+            foreach (var file in setOfFiles)
+            {
+                response += $"{file} -> file";
+            }
+
+            await writer.WriteAsync(response);
+        }
+
+        private async Task Get(string path, StreamWriter writer)
+        {
+            if (!File.Exists(path))
+            {
+                await writer.WriteLineAsync("-1");
+                return;
+            }
+        }
+
     }
 }
