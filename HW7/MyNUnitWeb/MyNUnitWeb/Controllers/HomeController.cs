22023e42e829c0aa8c50559c888cf238dd14125b
MihailBeloshapkin <misha.beloshapkin@yandex.ru>
2021-03-31 15:33:23 +0300

    create

---
+++ HW7/MyNUnitWeb/MyNUnitWeb/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyNUnitWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyNUnitWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+            => View("MyView");
+    }
+}
